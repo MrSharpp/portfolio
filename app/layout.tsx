496ec286d6acdafc97104b8c7560b412ef2d1a96
Amir Alam <40666775+MrSharpp@users.noreply.github.com>
2023-12-17 17:57:35 +0530

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,15 +10,15 @@ import { PreloadResources } from './preload';
 export const metadata: Metadata = {
   metadataBase: new URL('https://leerob.io'),
   title: {
-    default: 'Lee Robinson',
-    template: '%s | Lee Robinson',
+    default: 'Amir Alam',
+    template: '%s | Amir Alam',
   },
   description: 'Developer, writer, and creator.',
   openGraph: {
-    title: 'Lee Robinson',
+    title: 'Amir ALam',
     description: 'Developer, writer, and creator.',
     url: 'https://leerob.io',
-    siteName: 'Lee Robinson',
+    siteName: 'Amir ALam',
     locale: 'en_US',
     type: 'website',
   },
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: 'Lee Robinson',
+    title: 'Amir Alam',
     card: 'summary_large_image',
   },
   verification: {
