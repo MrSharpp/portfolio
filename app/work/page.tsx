4036f429b00e7e025d191b0376b99a35c838ccb3
Amir Alam <40666775+MrSharpp@users.noreply.github.com>
2023-12-18 23:01:09 +0530

    Update page.tsx

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighte">🕸️ Projects</h1>
       <p className="prose prose-neutral dark:prose-invert">
         This is where your work experience goes.
       </p>
